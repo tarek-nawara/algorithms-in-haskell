abee46d05a2a542781399129124de76b83ef2e54
tarek-nawara <tarek.nawara@gmail.com>
2018-02-20 22:11:50 +0200

    added bfs implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -29,28 +29,61 @@ data Vertex = Vertex
 neighbors :: Graph -> Vertex -> [Vertex]
 neighbors g v = map (\vid -> g Map.! vid) (adj v)
 
+-- Change the marked state of a vertex
+changeState :: Vertex -> IO ()
+changeState v = modifyIORef (marked v) not
+
+-- Check wither the vertex is marked or not
+shouldVisit :: Vertex -> IO Bool
+shouldVisit v = do
+  isMarked <- readIORef (marked v)
+  return (not isMarked)
+
+-- Reset the state of all vertices of the graph
+resetGraph :: Graph -> IO ()
+resetGraph g = mapM_ (\(_, v) -> changeState v) (Map.toList g)
+  
 -- | Implementation of depth first algorithm
 --   given the graph and a source vertex. 
 dfs :: Graph -> (Vertex -> a) -> Vertex -> IO [a]
-dfs g consumer start = do
-  modifyIORef (marked start) not
-  children    <- filterM shouldVisit (neighbors g start)
+dfs g consumer source = do
+  changeState source
+  children    <- filterM shouldVisit (neighbors g source)
   childrenRes <- mapM (dfs g consumer) children
-  return $ consumer start : concat childrenRes
-  where
-    shouldVisit v = do
-      isMarked <- readIORef (marked v)
-      return (not isMarked)
+  return $ consumer source : concat childrenRes
 
+-- | Implementation of breadth first algorithm
+--   given the graph and the source vertex.
+bfs :: Graph -> (Vertex -> a) -> Vertex -> IO [a]
+bfs g consumer source = bfsInner g consumer [source]
+  where
+    bfsInner _ _ [] = return []
+    bfsInner g consumer (source:rest) = do
+      changeState source
+      children <- filterM shouldVisit (neighbors g source)
+      restRes  <- bfsInner g consumer (rest ++ children)
+      return $ consumer source : restRes
+  
 -- TODO: this function should be removed
 someFunc :: IO ()
 someFunc = do
-  sourceMarked <- newIORef False
-  sinkMarked   <- newIORef False
-  let source   =  Vertex { vid = 1, marked = sourceMarked, adj = [2] }
-      sink     =  Vertex { vid = 2, marked = sinkMarked,   adj = [1] }
-      g        =  Map.fromList [(1, source), (2, sink)]
-  visitOrder   <- dfs g vid source
-  print visitOrder
+  oneMarked    <- newIORef False
+  twoMarked    <- newIORef False
+  threeMarked  <- newIORef False
+  fourMarked   <- newIORef False
+  fiveMarked   <- newIORef False
+  let vOne     =  Vertex { vid = 1, marked = oneMarked,   adj = [2, 3] }
+      vTwo     =  Vertex { vid = 2, marked = twoMarked,   adj = [5]    }
+      vThree   =  Vertex { vid = 3, marked = threeMarked, adj = [4]    }
+      vFour    =  Vertex { vid = 4, marked = fourMarked,  adj = []     }
+      vFive    =  Vertex { vid = 5, marked = fiveMarked,  adj = []     }
+      g        =  Map.fromList [(1, vOne), (2, vTwo), (3, vThree), (4, vFour), (5, vFive)]
+  bfsOrder    <- bfs g vid vOne
+  resetGraph g
+  dfsOrder    <- dfs g vid vOne
+  putStr "BFS visit order: "
+  print bfsOrder
+  putStr "DFS visit order: "
+  print dfsOrder
   
   
